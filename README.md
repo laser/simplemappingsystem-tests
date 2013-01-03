a00aadc569d7ba10261327776b6a2232272ae70a
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2013-01-03 08:44:09 -0800

    1) Seeding db with a test user

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ simplemappingsystem-tests
 =========================
 
 Written in python to test your simplemappingsystem server impl through the JSON-RPC layer. Swap out your server impl and run the tests - all must pass!
+
+
+Running the tests
+-----------------
+
+nosetests -v tests.py
--- tests.py
+++ tests.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python
+
+import barrister
+import logging
+
+# disable debug logging
+logging.getLogger("barrister").setLevel(logging.INFO)
+
+# change this to point at wherever your server is running
+trans = barrister.HttpTransport("http://localhost:3002/api-new")
+
+# automatically connects to endpoint and loads IDL JSON contract
+client = barrister.Client(trans)
+
 class TestCase:
-    def setup(self):
-        x = 2
-
-    def teardown(self):
-        x = 1
-
-    def test(self):
-        assert 1 == 1
+    def testCanGetUserSettings(self):
+        r = client.SimpleMappingSystem.get_user_settings("test-1234")
+        assert r["user_id"] == "test-1234", "%s != %s" % (r["user_id"], "test-1234",)
 
 if __name__ == "__main__":
     obj = TestCase()
-    obj.setup()
-    obj.test()
-    obj.teardown()
+    obj.testCanGetUserSettings()
