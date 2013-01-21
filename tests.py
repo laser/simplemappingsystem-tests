e0f4052afa210955af404e7c47f7490939f9411e
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2013-01-20 18:53:31 -0800

    1) Now parodying "ProjectService" as per API change 2) API calls now accept an authorization token ID instead of user ID.

--- tests.py
+++ tests.py
@@ -13,7 +13,7 @@ trans = barrister.HttpTransport("http://localhost:3002/api")
 # automatically connects to endpoint and loads IDL JSON contract
 client = barrister.Client(trans)
 
-svc = client.SimpleMappingSystem
+svc = client.ProjectService
 
 def expectsRpcException(code):
     def decorator(function):
@@ -32,50 +32,46 @@ class TestLifecycle:
     
     def __init__(self):
         self.pid = None
-        self.uid = "test-1234"
+        self.atid = "test-token"
 
     def setUp(self):
-        r = svc.add_project(self.uid, "test-project")
+        r = svc.add_project(self.atid, "test-project")
         self.pid = r["project_id"]
 
     def tearDown(self):
-        r = svc.get_projects(self.uid)
+        r = svc.get_projects(self.atid)
         for project in r:
-            svc.delete_project(self.uid, project["project_id"])
+            svc.delete_project(self.atid, project["project_id"])
 
     def testUserSettingsLifecycle(self):
-        r = svc.get_user_settings(self.uid)
-        assert r["user_id"] == self.uid
+        svc.update_user_settings(self.atid, "EN_US", "DECIMAL", "IMPERIAL", "HYBRID")
 
-        svc.update_user_settings(self.uid, "EN_US", "DECIMAL", "IMPERIAL", "HYBRID")
-
-        r = svc.get_user_settings(self.uid)
-        assert r["user_id"] == self.uid
+        r = svc.get_user_settings(self.atid)
         assert r["default_language"] == "EN_US"
         assert r["default_gps_format"] == "DECIMAL"
         assert r["default_measurement_system"] == "IMPERIAL"
         assert r["default_google_map_type"] == "HYBRID"
 
     def testProjectLifecycle(self):
-        r = svc.get_projects(self.uid)
+        r = svc.get_projects(self.atid)
         assert len(r) == 1
 
-        r = svc.add_project(self.uid, "project-a")
+        r = svc.add_project(self.atid, "project-a")
         tmp_project_id = r["project_id"]
         assert "project_id" in r
         assert r["name"] == "project-a"
 
-        r = svc.get_projects(self.uid)
+        r = svc.get_projects(self.atid)
         assert len(r) == 2
 
-        svc.delete_project(self.uid, tmp_project_id)
-        r = svc.get_projects(self.uid)
+        svc.delete_project(self.atid, tmp_project_id)
+        r = svc.get_projects(self.atid)
         assert len(r) == 1
 
     def testFieldLifeCycle(self):
         # basic "core" fields will be created along
         # with the project
-        r = svc.get_position_fields(self.uid, self.pid, False, [])
+        r = svc.get_position_fields(self.atid, self.pid, False, [])
         assert len(set([f["name"] for f in r]).intersection({"core_icon", "core_latitude", "core_longitude"})) == 3
         
         # order matters
@@ -84,46 +80,46 @@ class TestLifecycle:
         assert r[2]["name"] == "core_longitude"
 
         # add an additional field and stash the id
-        r = svc.add_position_field(self.uid, self.pid, "STRING", "favorite_color")
+        r = svc.add_position_field(self.atid, self.pid, "STRING", "favorite_color")
         tmp_position_field_id = r["position_field_id"]
 
         # should now have four fields, with the new one last
-        r = svc.get_position_fields(self.uid, self.pid, False, [])
+        r = svc.get_position_fields(self.atid, self.pid, False, [])
         assert len(set([f["name"] for f in r]).intersection({"core_icon", "core_latitude", "core_longitude", "favorite_color"})) == 4
         assert r[3]["name"] == "favorite_color"
 
         # update the order of the fields and reload
         r[2], r[3] = r[3], r[2]
-        svc.update_position_fields(self.uid, r)
-        r = svc.get_position_fields(self.uid, self.pid, False, [])
+        svc.update_position_fields(self.atid, r)
+        r = svc.get_position_fields(self.atid, self.pid, False, [])
         assert r[2]["name"] == "favorite_color"
         assert r[3]["name"] == "core_longitude"
 
         # delete the custom one we created
-        svc.delete_position_field(self.uid, tmp_position_field_id)
+        svc.delete_position_field(self.atid, tmp_position_field_id)
 
         # back to 3
-        r = svc.get_position_fields(self.uid, self.pid, False, [])
+        r = svc.get_position_fields(self.atid, self.pid, False, [])
         assert len(set([f["name"] for f in r]).intersection({"core_icon", "core_latitude", "core_longitude"})) == 3
 
     def testPositionLifecycle(self):
         # should have no position properties yet
-        r = svc.search_positions(self.uid, self.pid, "")
+        r = svc.search_positions(self.atid, self.pid, "")
         assert len(r) == 0
 
         # add a position to project, including a property
         # for the field just created
-        r = svc.add_position(self.uid, self.pid, [{"name": "core_icon", "value": "faje.png"}, {"name": "core_latitude", "value": "40"}, {"name": "core_longitude", "value": "40"}])
+        r = svc.add_position(self.atid, self.pid, [{"name": "core_icon", "value": "faje.png"}, {"name": "core_latitude", "value": "40"}, {"name": "core_longitude", "value": "40"}])
         temp_position_id_a = r["position_id"]
 
         # should return back a position now
-        r = svc.search_positions(self.uid, self.pid, "")
+        r = svc.search_positions(self.atid, self.pid, "")
         assert r[0]["position_properties"][0]["name"] == "core_icon"
         assert r[0]["position_properties"][0]["value"] == "faje.png"
 
         # update the position, adding some properties and
         # updating the one previously-added
-        svc.update_position(self.uid, temp_position_id_a, [{
+        svc.update_position(self.atid, temp_position_id_a, [{
             "name": "core_latitude",
             "value": "14"
         }, {
@@ -132,66 +128,65 @@ class TestLifecycle:
         }])
 
         # batch-add one more position (and properties)
-        r = svc.add_positions(self.uid, self.pid, [{"position_properties":[{"name": "core_icon", "value": "faje.png"}, {"name": "core_latitude", "value": "60"}, {"name": "core_longitude", "value": "60"}]}])
+        r = svc.add_positions(self.atid, self.pid, [{"position_properties":[{"name": "core_icon", "value": "faje.png"}, {"name": "core_latitude", "value": "60"}, {"name": "core_longitude", "value": "60"}]}])
         temp_position_id_b = r[0]["position_id"]
 
         # should now have the one position we added
-        r = svc.search_positions(self.uid, self.pid, "")
+        r = svc.search_positions(self.atid, self.pid, "")
         assert len(r) == 2
         assert len(set([p["name"] for p in r[0]["position_properties"]]).intersection({"core_longitude", "core_latitude"})) == 2
 
         # delete positions we've added
-        svc.delete_position(self.uid, temp_position_id_a)
-        svc.delete_position(self.uid, temp_position_id_b)
+        svc.delete_position(self.atid, temp_position_id_a)
+        svc.delete_position(self.atid, temp_position_id_b)
 
         # should now be back to zero
-        r = svc.search_positions(self.uid, self.pid, "")
+        r = svc.search_positions(self.atid, self.pid, "")
         assert len(r) == 0
 
     def testProjectAccessLifecycle(self):
         # creating a project adds project access
-        r = svc.add_project(self.uid, "test-project-access")
+        r = svc.add_project(self.atid, "test-project-access")
         new_project_id = r["project_id"]
-        r = svc.get_project_access(self.uid, new_project_id)
+        r = svc.get_project_access(self.atid, new_project_id)
         assert len(r) == 1
-        assert r[0]["user_id"] == self.uid
         assert r[0]["access_type"] == "OWNER"
         owner_project_access_id = r[0]["project_access_id"]
 
         # add a public access row
-        svc.add_project_access(self.uid, new_project_id, "PUBLIC", "EN_US", "METRIC", "DECIMAL", "HYBRID", "", [])
-        svc.add_project_access(self.uid, new_project_id, "READONLY", "ES_LA", "METRIC", "DECIMAL", "HYBRID", "Welcome to the system", ["text@example.com"])
+        svc.add_project_access(self.atid, new_project_id, "PUBLIC", "EN_US", "METRIC", "DECIMAL", "HYBRID", "", [])
+        svc.add_project_access(self.atid, new_project_id, "READONLY", "ES_LA", "METRIC", "DECIMAL", "HYBRID", "Welcome to the system", ["text@example.com"])
 
         # load the project_access for the project again and verify
-        r2 = svc.get_project_access(self.uid, new_project_id)
+        r2 = svc.get_project_access(self.atid, new_project_id)
         assert len(r2) == 3
         
         # delete the public and invited-user
         access_ids = filter(lambda x: x != owner_project_access_id, set(p["project_access_id"] for p in r2))
         for id in access_ids:
-            svc.delete_project_access(self.uid, id)
+            svc.delete_project_access(self.atid, id)
         
-        r = svc.get_project_access(self.uid, new_project_id)
+        r = svc.get_project_access(self.atid, new_project_id)
         assert len(r) == 1
 
 class TestErrors:
 
     def __init__(self):
         self.pid = None
-        self.uid = "test-1234"
+        self.atid = "test-token"
     
     def setUp(self):
-        r = svc.add_project(self.uid, "test-project")
+        r = svc.add_project(self.atid, "test-project")
         self.pid = r["project_id"]
 
     def tearDown(self):
-        r = svc.get_projects(self.uid)
+        r = svc.get_projects(self.atid)
         for project in r:
-            svc.delete_project(self.uid, project["project_id"])
+            svc.delete_project(self.atid, project["project_id"])
     
     @expectsRpcException(1002)
     def testMustSpecifyEmailAddressWhenAddingNonPublicAccessType(self):
-        svc.add_project_access(self.uid, self.pid, "COLLABORATOR", "EN_US", "METRIC", "DECIMAL", "HYBRID", "", [])
+        svc.add_project_access(self.atid, self.pid, "COLLABORATOR", "EN_US", "METRIC", "DECIMAL", "HYBRID", "", [])
     
     @expectsRpcException(1002)
     def testAddPositionRequiresCoreFieldValues(self):
@@ -206,19 +201,18 @@ class TestErrors:
             "value": ""
         }]
 
-        svc.add_position(self.uid, self.pid, create_position_properties)
+        svc.add_position(self.atid, self.pid, create_position_properties)
     
     @expectsRpcException(1004)   
     def testUnableToRemoveOwnerProjectAccess(self):
-        r = svc.get_project_access(self.uid, self.pid)
+        r = svc.get_project_access(self.atid, self.pid)
         assert r[0]["access_type"] == "OWNER"
-        assert r[0]["user_id"] == self.uid
 
-        svc.delete_project_access(self.uid, r[0]["project_access_id"])
+        svc.delete_project_access(self.atid, r[0]["project_access_id"])
 
     @expectsRpcException(1004)
     def testUnableToAddOwnerProjectAccess(self):
-        svc.add_project_access(self.uid, self.pid, "OWNER", "EN_US", "METRIC", "DECIMAL", "HYBRID", "", ["test@example.com"])
+        svc.add_project_access(self.atid, self.pid, "OWNER", "EN_US", "METRIC", "DECIMAL", "HYBRID", "", ["test@example.com"])
 
     @expectsRpcException(1004)
     def testAddPositionRequiresCoreField(self):
@@ -230,7 +224,7 @@ class TestErrors:
             "value": "45"
         }]
 
-        svc.add_position(self.uid, self.pid, create_position_properties)
+        svc.add_position(self.atid, self.pid, create_position_properties)
 
     @expectsRpcException(1004)
     def testMustAddCustomFieldsBeforeProperties(self):
@@ -248,5 +242,5 @@ class TestErrors:
             "value": "derp"
         }]
 
-        svc.add_position(self.uid, self.pid, create_position_properties)
+        svc.add_position(self.atid, self.pid, create_position_properties)
         
